c36b801974058a841d522a59e8b74019695b5ede
liqlin <liqlin@cn.ibm.com>
2020-10-27 17:08:00 +0800

    Create cp-proxy route
    Because the route resource of ingress nginx was created in the
    post install hook, this resource cannot be fully managed by operator.
    Move route resource in this operator which will act as parent operator
    to manage both route resources.

--- pkg/apis/addtoscheme_operator_v1alpha1.go
+++ pkg/apis/addtoscheme_operator_v1alpha1.go
@@ -19,7 +19,6 @@ import (
 	certmanager "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 	operator "github.com/openshift/api/operator/v1"
 	route "github.com/openshift/api/route/v1"
-	scc "github.com/openshift/api/security/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 
@@ -30,20 +29,17 @@ var (
 	// CertSchemeGroupVersion is group version used to register these objects
 	CertSchemeGroupVersion     = schema.GroupVersion{Group: "certmanager.k8s.io", Version: "v1alpha1"}
 	RouteSchemeGroupVersion    = schema.GroupVersion{Group: "route.openshift.io", Version: "v1"}
-	SCCSchemeGroupVersion      = schema.GroupVersion{Group: "security.openshift.io", Version: "v1"}
 	OperatorSchemeGroupVersion = schema.GroupVersion{Group: "operator.openshift.io", Version: "v1"}
 
 	// CertSchemeBuilder is used to add go types to the GroupVersionKind scheme
 	CertSchemeBuilder     = &scheme.Builder{GroupVersion: CertSchemeGroupVersion}
 	RouteSchemeBuilder    = &scheme.Builder{GroupVersion: RouteSchemeGroupVersion}
-	SCCSchemeBuilder      = &scheme.Builder{GroupVersion: SCCSchemeGroupVersion}
 	OperatorSchemeBuilder = &scheme.Builder{GroupVersion: OperatorSchemeGroupVersion}
 )
 
 func init() {
 	CertSchemeBuilder.Register(&certmanager.Certificate{})
 	RouteSchemeBuilder.Register(&route.Route{})
-	SCCSchemeBuilder.Register(&scc.SecurityContextConstraints{})
 	OperatorSchemeBuilder.Register(&operator.IngressController{})
 	OperatorSchemeBuilder.Register(&operator.IngressControllerList{})
 	OperatorSchemeBuilder.Register(&operator.DNS{})
@@ -51,5 +47,5 @@ func init() {
 
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and bac
 	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme, CertSchemeBuilder.AddToScheme,
-		RouteSchemeBuilder.AddToScheme, SCCSchemeBuilder.AddToScheme, OperatorSchemeBuilder.AddToScheme)
+		RouteSchemeBuilder.AddToScheme, OperatorSchemeBuilder.AddToScheme)
 }
